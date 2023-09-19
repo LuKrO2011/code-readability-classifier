9d9dc4fbcfbb92daf6f1a02eb98873e2ba6a3d57
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-09-19 20:29:25 +0200

    Creating dataset-creator.py file

---
+++ src/readability_classifier/data_loaders/dataset-creator.py
@@ -0,0 +1,9 @@
+# TODO: Load the own dataset:
+# Codename: {well_readable: code, badly_readable: code}
+
+# Well_readable code is in Methods_3 and is structured like this:
+# GitRepoName/ClassName/MethodName
+
+# Badly_readable code is in rdh_methods_15 and structured like this:
+# TopPackeName/ClassName/MethodName
+# where TopPackageName is for example "com" or "org"
