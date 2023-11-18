b56a0fb22287cc46d8bb1b132b44469b989c4bfc
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-11-18 09:14:17 +0100

    Making semantic_extractor.py the same as in Towards paper again

--- src/readability_classifier/models/extractors/semantic_extractor.py
+++ src/readability_classifier/models/extractors/semantic_extractor.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import torch
@@ -5,101 +6,119 @@ from torch import nn as nn
 from transformers import BertConfig
 
 from readability_classifier.models.base_model import BaseModel
+from readability_classifier.utils.config import SemanticInput
+from readability_classifier.utils.utils import load_yaml_file
+
+CURR_DIR = Path(os.path.dirname(os.path.relpath(__file__)))
+DEFAULT_SAVE_PATH = CURR_DIR / Path("../../../models/")
+CONFIGS_PATH = CURR_DIR / Path("../../res/models/")
 
 
 class TowardsBertConfig(BertConfig):
     """
     Configuration class to store the configuration of a `BertEmbedding`.
+    Same as in TowardsBert paper.
     """
 
     def __init__(self, **kwargs):
-        super().__init__(
-            vocab_size=kwargs.pop("vocab_size", 30000),
-            type_vocab_size=kwargs.pop("type_vocab_size", 300),
-            hidden_size=kwargs.pop("hidden_size", 768),
-            num_hidden_layers=kwargs.pop("num_hidden_layers", 12),
-            num_attention_heads=kwargs.pop("num_attention_heads", 12),
-            intermediate_size=kwargs.pop("intermediate_size", 3072),
-            hidden_activation=kwargs.pop("hidden_activation", "gelu"),
-            hidden_dropout_rate=kwargs.pop("hidden_dropout_rate", 0.1),
-            attention_dropout_rate=kwargs.pop("attention_dropout_rate", 0.1),
-            max_position_embeddings=kwargs.pop("max_position_embeddings", 200),
-            max_sequence_length=kwargs.pop("max_sequence_length", 100),
-            **kwargs,
-        )
+        super().__init__(**kwargs)
+        self.vocab_size = kwargs.get("vocab_size", 30000)  # 28996
+        self.type_vocab_size = kwargs.get("type_vocab_size", 300)
+        self.hidden_size = kwargs.get("hidden_size", 768)
+        self.num_hidden_layers = kwargs.get("num_hidden_layers", 12)
+        self.num_attention_heads = kwargs.get("num_attention_heads", 12)
+        self.intermediate_size = kwargs.get("intermediate_size", 3072)
+        self.hidden_activation = kwargs.get("hidden_activation", "gelu")
+        self.hidden_dropout_rate = kwargs.get("hidden_dropout_rate", 0.1)
+        self.attention_dropout_rate = kwargs.get("attention_dropout_rate", 0.1)
+        self.max_position_embeddings = kwargs.get("max_position_embeddings", 200)  # 512
+        self.max_sequence_length = kwargs.get("max_sequence_length", 100)
+
+    @classmethod
+    def build_config(cls) -> "TowardsBertConfig":
+        """
+        Build the model from a config.
+        :return: Returns the model.
+        """
+        return cls(**cls._get_config())
+
+    @classmethod
+    def _get_config(cls) -> dict[str, ...]:
+        """
+        Get the config hyperparams.
+        :return: The hyperparams.
+        """
+        return load_yaml_file(CONFIGS_PATH / f"{cls.__name__.lower()}.yaml")
 
 
-class BertEmbedding(BaseModel):
+class BertEmbedding(nn.Module):
     """
-    A Bert embedding layer.
+    A Bert embedding layer similar to the one used in the TowardsBert paper.
+    The embedding weights are trained from scratch!
     """
 
     def __init__(self, config: TowardsBertConfig) -> None:
         super().__init__()
-        self.model_name = "bert-base-cased"
-        self.vocab_size = config.vocab_size
-        self.hidden_size = config.hidden_size
 
-        # Token embedding
-        self.token_embedding = nn.Embedding(self.vocab_size, self.hidden_size)
+        # Specify device
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+        # Initialize embeddings
+        self.token_embedding = nn.Embedding(config.vocab_size, config.hidden_size)
         self.type_vocab_size = config.type_vocab_size
-
-        # Position embeddings
         self.position_embedding = nn.Embedding(
             config.max_position_embeddings, config.hidden_size
         )
-
-        # Token type embeddings
         self.token_type_embedding = nn.Embedding(
             config.type_vocab_size, config.hidden_size
         )
-
         self.layer_norm = nn.LayerNorm(config.hidden_size, eps=1e-12)
         self.dropout = nn.Dropout(config.hidden_dropout_rate)
-
-        # Initialize embeddings
         self.token_embedding.weight.data.normal_(mean=0.0, std=0.02)
         self.position_embedding.weight.data.normal_(mean=0.0, std=0.02)
         self.token_type_embedding.weight.data.normal_(mean=0.0, std=0.02)
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        # Send the embeddings to the GPU
+        self.layer_norm.to(self.device)
+        self.dropout.to(self.device)
+        self.token_embedding.to(self.device)
+        self.position_embedding.to(self.device)
+        self.token_type_embedding.to(self.device)
+
+    def embed(
+        self, input_ids: torch.Tensor, token_type_ids: torch.Tensor
+    ) -> torch.Tensor:
         """
-        Forward pass of the model.
-        :param inputs: The input tensor.
-        :return: The output of the model.
+        Embed the input using Bert.
+        :param input_ids: The token input tensor.
+        :param token_type_ids: The segment input tensor.
+        :return:
         """
-        input_ids, token_type_ids = inputs
         batch_size, sequence_length = input_ids.size()
 
-        # Generate position_ids within the defined range using modulo operation
+        # Create position ids
         position_ids = torch.arange(sequence_length, dtype=torch.long).unsqueeze(0)
-        position_ids = position_ids % self.position_embedding.num_embeddings
+        position_ids = position_ids % self.position_embedding.weight.size(0)
 
-        # Expand the input ids to the same size as position ids
+        # Create token type ids
         if token_type_ids is None:
             token_type_ids = torch.full_like(input_ids, 0)
 
-        # Embeddings
+        # Move input tensors to GPU
+        input_ids = input_ids.to(self.device)
+        token_type_ids = token_type_ids.cuda(self.device)
+
+        # Get embeddings
         position_embeddings = self.position_embedding(position_ids.to(input_ids.device))
         token_type_embeddings = self.token_type_embedding(token_type_ids)
         token_embeddings = self.token_embedding(input_ids)
 
-        # Sum all embeddings
+        # Sum all embeddings and apply layer norm and dropout
         embeddings = token_embeddings + token_type_embeddings + position_embeddings
         embeddings = self.layer_norm(embeddings)
         embeddings = self.dropout(embeddings)
-        return embeddings
 
-    @classmethod
-    def _build_from_config(cls, params: dict[str, ...], save: Path) -> "BaseModel":
-        """
-        Build the model from a config.
-        :param params: The config.
-        :param save: The path to save the model.
-        :return: Returns the model.
-        """
-        return cls(TowardsBertConfig(**params))
+        return embeddings
 
 
 class SemanticExtractorConfig:
@@ -127,7 +146,7 @@ class SemanticExtractor(BaseModel):
         """
         super().__init__()
 
-        self.bert_embedding = BertEmbedding.build_from_config()
+        self.bert_embedding = BertEmbedding(TowardsBertConfig.build_config())
 
         # In paper: kernel size not specified
         self.conv1 = nn.Conv1d(
@@ -146,16 +165,17 @@ class SemanticExtractor(BaseModel):
         # In paper: args not specified
         self.bidirectional_lstm = nn.LSTM(32, 32, bidirectional=True, batch_first=True)
 
-    def forward(
-        self, token_input: torch.Tensor, segment_input: torch.Tensor
-    ) -> torch.Tensor:
+    def forward(self, x: SemanticInput) -> torch.Tensor:
         """
         Forward pass of the model.
-        :param token_input: The token input tensor.
-        :param segment_input: The segment input tensor.
+        :param x: The input of the model.
         :return: The output of the model.
         """
-        texture_embedded = self.bert_embedding([token_input, segment_input])
+        input_ids = x.input_ids
+        token_type_ids = x.token_type_ids
+        # attention_mask is unused
+
+        texture_embedded = self.bert_embedding.embed(input_ids, token_type_ids)
 
         # Permute the tensor to fit the convolutional layer
         texture_embedded = texture_embedded.permute(0, 2, 1)
