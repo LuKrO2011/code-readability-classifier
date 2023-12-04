a66af09ca7f844d339fa85314d895f9a4d2d9f30
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-12-04 21:24:56 +0100

    Removing outdated test

--- tests/readability_classifier/keras/history_processing_test.py
+++ tests/readability_classifier/keras/history_processing_test.py
@@ -1,7 +1,3 @@
-import json
-import pickle
-from dataclasses import asdict
-
 import pytest
 
 from readability_classifier.keras.history_processing import HistoryProcessor
@@ -10,27 +6,3 @@ from readability_classifier.keras.history_processing import HistoryProcessor
 @pytest.fixture()
 def processor():
     return HistoryProcessor()
-
-
-def test_process(processor):
-    input_path = "res/keras/history/legacy_history.pkl"
-    compare_path = "res/keras/history/processed_legacy_history.json"
-
-    # Load the history
-    with open(input_path, "rb") as file:
-        loaded_history = pickle.load(file)
-
-    # Process the history
-    stats = HistoryProcessor().evaluate(loaded_history)
-    stats = asdict(stats)
-
-    # # Store the stats as json
-    # with open(store_path, "w") as file:
-    #     json.dump(stats, file, indent=4)
-
-    # Load the compare stats
-    with open(compare_path) as file:
-        compare_stats = json.load(file)
-
-    # Compare the stats
-    assert stats == compare_stats
