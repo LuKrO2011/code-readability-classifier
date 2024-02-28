d42790eb9d5998f20d4cc2e1996148ae186ef35e
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-28 13:34:10 +0100

    Adding important notes to modules

---
+++ src/readability_classifier/keas/__init__.py
@@ -0,0 +1,1 @@
+# Do not change the name of "keas" to "kreas" as this would break import statements.
---
+++ src/readability_classifier/toch/__init__.py
@@ -0,0 +1,3 @@
+# DeprecationWarning: The torch module is not used anymore as the results of the keas
+# version could not be reproduced.
+# Do not change the name of "toch" to "torch" as this would break import statements.
