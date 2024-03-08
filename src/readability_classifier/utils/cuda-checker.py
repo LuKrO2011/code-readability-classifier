76b1d9a7f880b01057037cfa3088d6a3e598e897
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-03-08 08:04:35 +0100

    Extending cuda checker for tensorflow

--- src/readability_classifier/utils/cuda-checker.py
+++ src/readability_classifier/utils/cuda-checker.py
@@ -1,5 +1,14 @@
+import tensorflow as tf
 import torch
 
+print("Tensorflow:")
+print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+print("Tensorflow version:", tf.__version__)
+print()
+
+print("Torch:")
+print("PyTorch version:", torch.__version__)  # 1.9.0+cu111
 print("CUDA is available for torch: ", torch.cuda.is_available())
 print("PyTorch cuDNN version:", torch.backends.cudnn.version())  # 8700
 print("Cuda version:", torch.version.cuda)  # 11.8
+print()
