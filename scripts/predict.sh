36d47e65c266e6da3e83fb1555785395d3dc3805
Lukas Krodinger <lukas.krodinger@gmail.com>
2024-06-04 14:27:28 +0200

    Moving scripts to dedicated folder

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -3,10 +3,10 @@
 podman run --rm \
     --device nvidia.com/gpu=all \
     --security-opt=label=disable \
-    -v ./src:/app/src \
-    -v ./tests/res/models:/app/models \
-    -v ./tests/res/code_snippets:/app/snippets \
-    rc-gpu \
+    -v ../src:/app/src \
+    -v ../tests/res/models:/app/models \
+    -v ../tests/res/code_snippets:/app/snippets \
+    docker.io/lukro2011/rc-gpu:latest \
     python src/readability_classifier/main.py PREDICT \
     --model models/towards.keras \
     --input snippets/towards.java
--- scripts/train.sh
+++ scripts/train.sh
@@ -3,10 +3,10 @@
 podman run --rm \
     --device nvidia.com/gpu=all \
     --security-opt=label=disable \
-    -v ./src:/app/src \
-    -v ./tests/res/raw_datasets/combined:/app/res \
-    -v ./output:/app/output \
-    rc-gpu \
+    -v ../src:/app/src \
+    -v ../tests/res/raw_datasets/combined:/app/res \
+    -v ../output:/app/output \
+    docker.io/lukro2011/rc-gpu:latest \
     python src/readability_classifier/main.py TRAIN \
     -i res \
     -s output
